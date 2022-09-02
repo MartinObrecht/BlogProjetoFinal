71ee15e278cf9e816ba66b398baade875d666f85
Martin Obrecht <martin.obrecht@yahoo.com.br>
2022-09-02 20:21:02 -0300

    fix: ajuste controller

--- BlogProjetoFinal.WebApi/Controllers/ArtigoController.cs
+++ BlogProjetoFinal.WebApi/Controllers/ArtigoController.cs
@@ -14,66 +14,66 @@ using System;
 
 namespace BlogProjetoFinal.WebApi.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ArtigoController : LiquidControllerBase
-    {
-        public ArtigoController(IMediator mediator) : base(mediator)
-        {
-        }
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class ArtigoController : LiquidControllerBase
+//    {
+//        public ArtigoController(IMediator mediator) : base(mediator)
+//        {
+//        }
 
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+//        [HttpGet("{id}")]
+//        [ProducesResponseType(StatusCodes.Status200OK)]
 
-        public async Task<IActionResult> Get([FromRoute] int id)
-        {
-            var response = await ExecuteAsync(new ReadArtigoQuery(id));
+//        public async Task<IActionResult> Get([FromRoute] int id)
+//        {
+//            var response = await ExecuteAsync(new ReadArtigoQuery(id));
 
-            if (response.Data == null) return NotFound();
+//            if (response.Data == null) return NotFound();
 
-            return Ok(response.Data);
-        }
+//            return Ok(response.Data);
+//        }
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> Get()
-        {
-            var response = await ExecuteAsync(new ListArtigoQuery());
+//        [HttpGet]
+//        [ProducesResponseType(StatusCodes.Status200OK)]
+//        public async Task<IActionResult> Get()
+//        {
+//            var response = await ExecuteAsync(new ListArtigoQuery());
 
-            if (response.Data == null) return NotFound();
+//            if (response.Data == null) return NotFound();
 
-            return Ok(response.Data);
-        }
+//            return Ok(response.Data);
+//        }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> Post([FromBody] ArtigoEntity entity)
-        {
-            await ExecuteAsync(new CreateArtigoCommand(entity));
+//        [HttpPost]
+//        [ProducesResponseType(StatusCodes.Status201Created)]
+//        public async Task<IActionResult> Post([FromBody] ArtigoEntity entity)
+//        {
+//            await ExecuteAsync(new CreateArtigoCommand(entity));
 
-            return CreatedAtRoute("", new { id = entity.Id }, entity);
-        }
+//            return CreatedAtRoute("", new { id = entity.Id }, entity);
+//        }
 
-        [HttpPut]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> Put([FromBody] ArtigoEntity entity)
-        {
-            var response = await ExecuteAsync(new UpdateArtigoCommand(entity));
+//        [HttpPut]
+//        [ProducesResponseType(StatusCodes.Status204NoContent)]
+//        public async Task<IActionResult> Put([FromBody] ArtigoEntity entity)
+//        {
+//            var response = await ExecuteAsync(new UpdateArtigoCommand(entity));
 
-            if (response.Data == null) return NotFound();
+//            if (response.Data == null) return NotFound();
 
-            return NoContent();
-        }
+//            return NoContent();
+//        }
 
-        [HttpDelete("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> Delete([FromRoute] int id)
-        {
-            var response = await ExecuteAsync(new RemoveArtigoCommand(id));
+//        [HttpDelete("{id}")]
+//        [ProducesResponseType(StatusCodes.Status204NoContent)]
+//        public async Task<IActionResult> Delete([FromRoute] int id)
+//        {
+//            var response = await ExecuteAsync(new RemoveArtigoCommand(id));
 
-            if (response.Data == null) return NotFound();
+//            if (response.Data == null) return NotFound();
 
-            return NoContent();
-        }
-    }
+//            return NoContent();
+//        }
+//    }
 }
